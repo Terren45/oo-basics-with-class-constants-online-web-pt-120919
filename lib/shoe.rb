9836c175b2bf5f14d8054655e11fa9d7792bf241
Terren Culberson <culbersonterren@yahoo.com>
2020-01-06 21:46:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ BRANDS = []
   
   def initialize(brand)
     @brand = brand
-    
+     if !(BRANDS.include?(@brand))
     BRANDS << brand
   end
 
@@ -14,9 +14,9 @@ BRANDS = []
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-   def brand=(brand)
-     @brand = brand
-     BRANDS << brand
+   #def brand=(brand)
+    # @brand = brand
+     #BRANDS << brand
    end
    
 
