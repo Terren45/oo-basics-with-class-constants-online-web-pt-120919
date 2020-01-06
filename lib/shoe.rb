45b27522750d09e67a1da4e4eeb618c11bfc51b7
Terren Culberson <culbersonterren@yahoo.com>
2020-01-06 21:32:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand 
 
-BRAND = []
+BRANDS = []
   
   def initialize(brand)
     @brand = brand
@@ -14,7 +14,7 @@ BRAND = []
   end
    def brand=(brand)
      @brand = brand
-     BRAND << brand
+     BRANDS << brand
    end
    
 
